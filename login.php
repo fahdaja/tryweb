60baedceacf962bf9b57fc7edd6aa82a9a05c81f
mochanoerfaizi <noerfaizim@gmail.com>
2024-05-30 13:35:04 +0700

    push

--- login.php
+++ login.php
@@ -45,8 +45,8 @@ if (isset($_POST["login"])) {
 
 <head>
   <meta charset="utf-8">
-  <title>Login Sport Center</title>
-  <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous"> -->
+  <title>Login My futsal Center</title>
+  
   <link rel="stylesheet" href="style.css">
 </head>
 
